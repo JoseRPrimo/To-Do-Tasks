3a88fd1fb4c217458a0a0121de3aa163725fcaa9
joserprimo <jose.primo.dev@gmail.com>
2026-01-28 17:09:22 -0300

    First Commit

---
+++ src/main/java/JoseRPrimo/todo/Controller/TaskController.java
@@ -0,0 +1,12 @@
+package JoseRPrimo.todo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class TaskController {
+
+}
