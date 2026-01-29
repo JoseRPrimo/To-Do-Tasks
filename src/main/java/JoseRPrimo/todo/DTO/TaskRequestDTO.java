b73c4e8b74dfe397289b8fb2eed88902c8fb93eb
joserprimo <jose.primo.dev@gmail.com>
2026-01-29 19:03:57 -0300

    Implementação do swagger no TaskRequestDTO, TaskResponseDTO

--- src/main/java/JoseRPrimo/todo/DTO/TaskRequestDTO.java
+++ src/main/java/JoseRPrimo/todo/DTO/TaskRequestDTO.java
@@ -1,5 +1,6 @@
 package JoseRPrimo.todo.DTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -8,9 +9,13 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Dados para criação ou atualização de uma task")
 public class TaskRequestDTO {
     @NotBlank(message = "Titulo Obrigatorio")
+    @Schema(description = "Titulo da task", example = "Regar as plantas")
     private String titulo;
+    @Schema(description = "Descricao da task", example = "Regar todas as plantas do quintal")
     private String descricao;
+    @Schema(description = "Informa se a task foi ou não concluida", example = "true")
     private Boolean concluida;
 }
--- src/main/java/JoseRPrimo/todo/DTO/TaskResponseDTO.java
+++ src/main/java/JoseRPrimo/todo/DTO/TaskResponseDTO.java
@@ -1,4 +1,5 @@
 package JoseRPrimo.todo.DTO;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,10 +8,16 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 
+@Schema(description = "Dados retornados pelo sistema para leitura", accessMode = Schema.AccessMode.READ_ONLY)
 public class TaskResponseDTO {
+    @Schema(description = "ID da task", example = "1")
     private Long id;
+    @Schema(description = "Título da task", example = "Regar as plantas")
     private String titulo;
+    @Schema(description = "Descrição da task", example = "Regar todas as plantas do quintal")
     private String descricao;
+    @Schema(description = "Informa se a task foi ou não concluída")
     private Boolean concluida;
+    @Schema(description = "Data de criação da task", example = "2024-01-30", type = "string", format = "date")
     private LocalDate dataCriacao;
 }
